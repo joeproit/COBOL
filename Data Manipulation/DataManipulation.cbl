9a1992c628584dfe15c9a2f0739bc3509dd57ab9
JoePro <git@joeproit.com>
2023-05-15 22:10:24 -0500

    Update DataManipulation.cbl
    include error handling around file operations and user input

--- Data Manipulation/DataManipulation.cbl
+++ Data Manipulation/DataManipulation.cbl
@@ -5,7 +5,8 @@ ENVIRONMENT DIVISION.
 INPUT-OUTPUT SECTION.
 FILE-CONTROL.
     SELECT ReceiptFile ASSIGN TO 'RECEIPT.DAT'
-           ORGANIZATION IS LINE SEQUENTIAL.
+           ORGANIZATION IS LINE SEQUENTIAL
+           FILE STATUS IS WS-FileStatus.
 
 DATA DIVISION.
 FILE SECTION.
@@ -20,18 +21,31 @@ WORKING-STORAGE SECTION.
 01 UpdatedBalance PIC 9(7)V99 VALUE ZEROS.
 01 UpdatedBalanceString PIC X(10) VALUE SPACES.
 01 ReceiptString PIC X(80) VALUE SPACES.
+01 WS-FileStatus PIC 9(2) VALUE ZEROS.
 
 PROCEDURE DIVISION.
 Begin.
 
     DISPLAY "Enter the customer name: "
-    ACCEPT CustomerName.
+    ACCEPT CustomerName
+    IF FUNCTION NUMVAL-C(CustomerName) > 0
+        DISPLAY "Error: Customer name cannot be a number. Please try again."
+        STOP RUN
+    END-IF.
 
     DISPLAY "Enter the current account balance: "
-    ACCEPT AccountBalance.
+    ACCEPT AccountBalance
+    IF FUNCTION NUMVAL-C(AccountBalance) = 0
+        DISPLAY "Error: Invalid account balance. Please enter a number."
+        STOP RUN
+    END-IF.
 
     DISPLAY "Enter the deposit amount: "
-    ACCEPT DepositAmount.
+    ACCEPT DepositAmount
+    IF FUNCTION NUMVAL-C(DepositAmount) = 0
+        DISPLAY "Error: Invalid deposit amount. Please enter a number."
+        STOP RUN
+    END-IF.
 
     * Arithmetic operations
     COMPUTE UpdatedBalance = AccountBalance + DepositAmount
@@ -47,8 +61,23 @@ Begin.
 
     * Write the receipt to a file
     OPEN OUTPUT ReceiptFile
+    IF WS-FileStatus NOT EQUAL ZERO
+        DISPLAY "Error: Unable to open RECEIPT.DAT. File Status: " WS-FileStatus
+        STOP RUN
+    END-IF.
+
     MOVE ReceiptString TO ReceiptRecord
     WRITE ReceiptRecord
+    IF WS-FileStatus NOT EQUAL ZERO
+        DISPLAY "Error: Unable to write to RECEIPT.DAT. File Status: " WS-FileStatus
+        CLOSE ReceiptFile
+        STOP RUN
+    END-IF.
+
     CLOSE ReceiptFile
+    IF WS-FileStatus NOT EQUAL ZERO
+        DISPLAY "Error: Unable to close RECEIPT.DAT. File Status: " WS-FileStatus
+        STOP RUN
+    END-IF.
 
     STOP RUN.
