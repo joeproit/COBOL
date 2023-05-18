23288a3ce8f8b46673b2fb81713867c2b20fe48e
JoePro <git@joeproit.com>
2023-05-17 19:54:41 -0500

    Update DataManipulation.cbl

--- Data Manipulation/DataManipulation.cbl
+++ Data Manipulation/DataManipulation.cbl
@@ -25,56 +25,59 @@ WORKING-STORAGE SECTION.
 
 PROCEDURE DIVISION.
 Begin.
+    MOVE ZEROS TO WS-FileStatus.
 
-    DISPLAY "Enter the customer name: "
-    ACCEPT CustomerName
+    DISPLAY "Enter the customer name: ".
+    ACCEPT CustomerName.
     IF FUNCTION LENGTH(CustomerName) = 0
         DISPLAY "Error: Customer name cannot be empty. Please try again."
         STOP RUN
     END-IF.
 
-    DISPLAY "Enter the current account balance: "
-    ACCEPT AccountBalance
-    IF FUNCTION NUMVAL-C(AccountBalance) = 0 OR AccountBalance < 0
-        DISPLAY "Error: Invalid account balance. Please enter a positive number."
-        STOP RUN
-    END-IF.
+    DISPLAY "Enter the current account balance: ".
+    ACCEPT AccountBalance.
+    EVALUATE TRUE
+        WHEN (FUNCTION NUMVAL-C(AccountBalance) = 0 OR AccountBalance < 0)
+            DISPLAY "Error: Invalid account balance. Please enter a positive number."
+            STOP RUN
+        WHEN OTHER
+            CONTINUE
+    END-EVALUATE.
 
-    DISPLAY "Enter the deposit amount: "
-    ACCEPT DepositAmount
-    IF FUNCTION NUMVAL-C(DepositAmount) = 0 OR DepositAmount < 0
-        DISPLAY "Error: Invalid deposit amount. Please enter a positive number."
-        STOP RUN
-    END-IF.
+    DISPLAY "Enter the deposit amount: ".
+    ACCEPT DepositAmount.
+    EVALUATE TRUE
+        WHEN (FUNCTION NUMVAL-C(DepositAmount) = 0 OR DepositAmount < 0)
+            DISPLAY "Error: Invalid deposit amount. Please enter a positive number."
+            STOP RUN
+        WHEN OTHER
+            CONTINUE
+    END-EVALUATE.
 
-    * Arithmetic operations
-    COMPUTE UpdatedBalance = AccountBalance + DepositAmount
+    COMPUTE UpdatedBalance = AccountBalance + DepositAmount.
 
-    * Data conversion
-    MOVE UpdatedBalance TO UpdatedBalanceString
+    MOVE UpdatedBalance TO UpdatedBalanceString.
 
-    * String manipulation
     STRING "Customer: " CustomerName
            ", Deposit Amount: " DepositAmount
            ", New Balance: " UpdatedBalanceString
-           INTO ReceiptString
+           INTO ReceiptString.
 
-    * Write the receipt to a file
-    OPEN OUTPUT ReceiptFile
+    OPEN OUTPUT ReceiptFile.
     IF WS-FileStatus NOT EQUAL ZERO
         DISPLAY "Error: Unable to open RECEIPT.DAT. File Status: " WS-FileStatus
         STOP RUN
     END-IF.
 
-    MOVE ReceiptString TO ReceiptRecord
-    WRITE ReceiptRecord
+    MOVE ReceiptString TO ReceiptRecord.
+    WRITE ReceiptRecord.
     IF WS-FileStatus NOT EQUAL ZERO
         DISPLAY "Error: Unable to write to RECEIPT.DAT. File Status: " WS-FileStatus
         CLOSE ReceiptFile
         STOP RUN
     END-IF.
 
-    CLOSE ReceiptFile
+    CLOSE ReceiptFile.
     IF WS-FileStatus NOT EQUAL ZERO
         DISPLAY "Error: Unable to close RECEIPT.DAT. File Status: " WS-FileStatus
         STOP RUN
